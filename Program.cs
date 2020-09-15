faa7b010c0928c19962f664b3d3a9607383b0c2b
Psycho20 <54379889+Psycho20@users.noreply.github.com>
2020-09-14 22:34:17 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,10 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tarefa_Pessoas_Programação
+namespace Tarefa_Poo_ProdutoAtualizado
 {
-    
-
     class Program
     {
 
@@ -15,49 +13,53 @@ namespace Tarefa_Pessoas_Programação
         static void Main(string[] args)
         {
 
-            string NomeA, NomeB;
-            int Sal1, Sal2, IdadeA, IdadeB;
-            int medSal;
-
-            Console.WriteLine("=======================================");
-            Console.WriteLine("Digite o Nome, idade e salário da pessoa 1:");
-            Console.Write("Nome: ");
-            NomeA = Console.ReadLine();
-            Console.Write("Idade: ");
-            IdadeA = int.Parse(Console.ReadLine());
-            Console.Write("Salário: ");
-            Sal1 = int.Parse(Console.ReadLine());
-            Console.ReadLine();
+            String nomeProd; //Nome do produto
+            float valor; //Valor do produto
+            int quantSoma; //Quantidade para somar
+            int quantSub; //Quantidade para subtrair
+            int quant; //Valor da quantidade
+            int totalQuant; //Total da soma da quantidade
+            int totalQuantSub; //Total da subtração da quantidade somada
+            float total; //Valor total do produto em relação a quantidade de produtos
+            float totalSom; //Valor total do produto após a soma
+            float totalSub; //Valor total do produto após a subtração depois da soma
             
 
-            Console.Clear();
-            Console.WriteLine("=======================================");
-            Console.WriteLine("Digite o Nome, idade e salário da pessoa 2:");
+            Console.WriteLine("Entre com os produtos");
             Console.Write("Nome: ");
-            NomeB = Console.ReadLine();
-            Console.Write("Idade: ");
-            IdadeB = int.Parse(Console.ReadLine());
-            Console.Write("Salário: ");
-            Sal2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("=======================================");
+            nomeProd = Console.ReadLine();
+            Console.Write("Preço: ");
+            valor = float.Parse(Console.ReadLine());
+            Console.Write("Quantidade em estoque: ");
+            quant = int.Parse(Console.ReadLine());
             Console.Clear();
 
-            medSal = (Sal1 + Sal2) / 2;
 
-            if (IdadeA > IdadeB)
-            {
-                Console.WriteLine("O nome da pessoa mais velha é " + NomeA);
-                Console.WriteLine("A média salarial é: " + medSal);
-                Console.ReadKey();
-            }
-            else
-            {
-                Console.WriteLine("O nome da pessoa mais velha é " + NomeB);
-                Console.WriteLine("A média salarial é: " + medSal);
-                Console.ReadKey();
-            }
+            total = valor * quant; //Dando o valor total depois de multiplicar o valor inserido do preço do produto com a quantidade de produto inserido
 
-            
+            Console.WriteLine("Dados do produto: " + nomeProd + ", R$" + valor + "," + quant + " Unidades" + " Total: " + total);
+            Console.WriteLine("-------------");
+
+            Console.WriteLine("Digite o número a ser adicionado no estoque: ");
+            quantSoma = int.Parse(Console.ReadLine());
+
+            totalSom = total + (quantSoma * valor); //Dando o valor total da quantidade adicional do produto depois de somar o total do preço do produto em relação a quantidade no estoque com a multiplicação da quantidade adicional com o valor do produto
+            totalQuant = quantSoma + quant; //Dando o valor total da quantidade como resultado total da soma da quantidade adicional com a quantidade que tinha anteriormente no estoque
+
+            Console.Clear();
+            Console.WriteLine("Dados atualizados: " + nomeProd + ", R$" + valor + "," + totalQuant + " Unidades" + " Total: " + totalSom);
+            Console.WriteLine("-------------");
+            Console.WriteLine("Digite o número a ser removido do estoque: ");
+            quantSub = int.Parse(Console.ReadLine());
+
+            totalSub = totalSom - (quantSub * valor); //Dando o valor total do valor do produto subtraindo o total da soma do valor em reais do estoque com a multiplicação da quantidade inserida pra ser removida do estoque com o valor de produto
+
+            totalQuantSub = totalQuant - quantSub; //Dando o valor total da quantidade da subtração da quantidade total anterior depois de ter adicionado mais produtos subtraindo com a quantidade inserida pra retirada do produto do estoque
+
+            Console.Clear();
+            Console.WriteLine("Dados atualizados: " + nomeProd + ", R$" + valor + "," + totalQuantSub + " Unidades" + " Total: " + totalSub);
+
+            Console.ReadKey();
 
         }
     }
